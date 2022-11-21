68b8140d4250bdea749be0aa4f3c1ed5eaa07d1f
dilanduman <dumandlan@gmail.com>
2022-11-21 11:57:04 +0300

    sayac olusturuldu

---
+++ app/src/main/java/com/alan/kotlincountdown/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.alan.kotlincountdown
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView:TextView=findViewById(R.id.textView)
+
+        object : CountDownTimer(10000,1000){
+            override fun onFinish() {
+                textView.text="Left: 0"
+            }
+
+            override fun onTick(milliUntilFinished: Long) {
+                textView.text="Left: ${milliUntilFinished/1000}"
+            }
+        }.start()
+
+
+    }
+}
